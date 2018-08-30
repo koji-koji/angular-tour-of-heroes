4848278f276a6c92fdfacbb1a616722a0e4ce3a2
koji-koji <k.koyama37@gmail.com>
2018-08-30 22:31:06 +0900

    add getHeroes method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,9 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['/heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-
 
   constructor(private heroService: HeroService) { }
 
