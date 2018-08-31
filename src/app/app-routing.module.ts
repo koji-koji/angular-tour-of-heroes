8dfbc5a33d40c07100be3f1a2ca5c1b2c8e97bf5
koji-koji <k.koyama37@gmail.com>
2018-08-31 16:38:26 +0900

    add mdb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HeroesComponent } from './heroes/heroes.component'
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [¥
+const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
