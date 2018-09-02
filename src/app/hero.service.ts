9f1fc9d3403de97ffe3f53d121485435d386274f
koji-koji <k.koyama37@gmail.com>
2018-09-02 21:54:14 +0900

    set handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,4 +34,12 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`)
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
 }
