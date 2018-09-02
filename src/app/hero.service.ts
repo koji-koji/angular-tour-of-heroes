08772f9bed05345e3f25c9184e4b1d616d3fa86e
koji-koji <k.koyama37@gmail.com>
2018-09-02 21:44:48 +0900

    set catchError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,8 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,11 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
+    // return this.http.get<Hero[]>(this.heroesUrl);
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
